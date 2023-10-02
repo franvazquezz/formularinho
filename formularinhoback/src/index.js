ffb65171ff38a5f297aee14b4ce86ec53723d1f5
franvazquezz <franvazquez2001@gmail.com>
2023-10-02 12:33:29 -0300

    deploy 2

--- formularinhoback/src/index.js
+++ formularinhoback/src/index.js
@@ -17,12 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./app.js');
-const { conn } = require('./db.js');
-
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+require ('dotenv').config() 
+const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
-conn.sync({ force: true, alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('Server listening on port 3001'); // eslint-disable-line no-console
+conn.sync({alter: true}).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
   });
 });
